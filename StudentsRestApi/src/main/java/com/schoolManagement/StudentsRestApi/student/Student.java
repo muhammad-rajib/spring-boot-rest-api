0f528627ea51e8f1f8a184add2dae76ce167c361
muhammad-rajib <rajibhossain8521@gmail.com>
2022-04-06 17:16:29 +0600

    added getter and setter in student class.

--- StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/Student.java
+++ StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/Student.java
@@ -36,4 +36,78 @@ public class Student {
         this.contact = contact;
         this.address = address;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(LocalDate birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public int getAdmit_year() {
+        return admit_year;
+    }
+
+    public void setAdmit_year(int admit_year) {
+        this.admit_year = admit_year;
+    }
+
+    public Long getContact() {
+        return contact;
+    }
+
+    public void setContact(Long contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                "name=" + name + '\'' +
+                "department=" + department + '\'' +
+                "admitYear=" + admit_year +
+                "}";
+    }
 }
