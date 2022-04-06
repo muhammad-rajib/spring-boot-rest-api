f8ca8c12c5f002be81322642b7eb620ec8613a3e
muhammad-rajib <rajibhossain8521@gmail.com>
2022-04-06 15:55:56 +0600

    Sprint Boot - Rest Api
    - Project Initialized

---
+++ StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/StudentsRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.schoolManagement.StudentsRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsRestApiApplication.class, args);
+	}
+
+}
---
+++ StudentsRestApi/src/test/java/com/schoolManagement/StudentsRestApi/StudentsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolManagement.StudentsRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
