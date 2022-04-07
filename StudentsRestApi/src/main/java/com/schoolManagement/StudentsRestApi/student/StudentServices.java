88318ae13429eb7ddecae4814134e09cbbaa732f
muhammad-rajib <rajibhossain8521@gmail.com>
2022-04-07 13:38:42 +0600

    added email exist logic
    - if email exist then throw exceptions
    - else save the new student into database

--- StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/StudentServices.java
+++ StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/StudentServices.java
@@ -3,7 +3,9 @@ package com.schoolManagement.StudentsRestApi.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -21,6 +23,12 @@ public class StudentServices {
     }
 
     public void addNewStudent(Student student) {
+
+        Optional<Student> studentOptional = studentDBRepository.findByEmail(student.getEmail());
+
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Email already exist");
+        }
         studentDBRepository.save(student);
     }
 }
