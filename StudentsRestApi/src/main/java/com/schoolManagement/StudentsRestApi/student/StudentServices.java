be193f67c551763e4f312c6d69b2ce9b16719de9
muhammad-rajib <rajibhossain8521@gmail.com>
2022-04-07 12:04:02 +0600

    removed unnecessary lines

--- StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/StudentServices.java
+++ StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/StudentServices.java
@@ -1,12 +1,8 @@
 package com.schoolManagement.StudentsRestApi.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.SequenceGenerator;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 
