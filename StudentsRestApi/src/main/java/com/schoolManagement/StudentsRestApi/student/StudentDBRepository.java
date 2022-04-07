32c23d4eb301a13f2c9a3eed0ae8187dc16ba73c
muhammad-rajib <rajibhossain8521@gmail.com>
2022-04-07 14:15:12 +0600

    Added Query
    - find student by email

--- StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/StudentDBRepository.java
+++ StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/StudentDBRepository.java
@@ -3,9 +3,17 @@ package com.schoolManagement.StudentsRestApi.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface StudentDBRepository extends JpaRepository<Student, Long> {
     /* this interface is responsible for data access from database */
 
+    // select * from student where email = <givenEmail>;
+    /*
+    * Optional is a new container type that wraps a single value, if the value is available.
+    * So it's meant to convey the meaning that the value might be absent.
+    */
+    Optional<Student> findByEmail(String email);
 }
