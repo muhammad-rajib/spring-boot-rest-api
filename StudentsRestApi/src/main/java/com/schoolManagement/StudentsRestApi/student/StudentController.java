398d1f9b5a9ae185e54a5623c0bd1560e3861bce
muhammad-rajib <rajibhossain8521@gmail.com>
2022-04-07 12:32:50 +0600

    added @RequestBody annotation in registerNewStudent() method
    - to take the new student from request body
    - and transfer it to the student class

--- StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/StudentController.java
+++ StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/StudentController.java
@@ -1,10 +1,7 @@
 package com.schoolManagement.StudentsRestApi.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,7 +34,7 @@ public class StudentController {
     }
 
     @PostMapping
-    public void registerNewStudent(Student student) {
+    public void registerNewStudent(@RequestBody Student student) {
         studentServices.addNewStudent(student);
     }
 
