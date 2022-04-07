ae6ee3382e4b4dfff3d8934da491ae1233e2ca01
muhammad-rajib <rajibhossain8521@gmail.com>
2022-04-07 10:47:48 +0600

    Updated getStudent() service
    - Used findAll() from studentDBRepository to return all student list
      from database.

--- StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/StudentServices.java
+++ StudentsRestApi/src/main/java/com/schoolManagement/StudentsRestApi/student/StudentServices.java
@@ -1,8 +1,10 @@
 package com.schoolManagement.StudentsRestApi.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.SequenceGenerator;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
@@ -11,18 +13,14 @@ import java.util.List;
 @Service
 public class StudentServices {
 
+    private final StudentDBRepository studentDBRepository;
+
+    @Autowired
+    public StudentServices(StudentDBRepository studentDBRepository) {
+        this.studentDBRepository = studentDBRepository;
+    }
+
     public List<Student> getStudent() {
-        return List.of(
-                new Student (
-                        1L,
-                        "Forid",
-                        "cmt",
-                        LocalDate.of(2000, Month.APRIL, 10),
-                        25,
-                        2019,
-                        123123,
-                        "Dhaka, Bangladesh"
-                )
-        );
+        return studentDBRepository.findAll();
     }
 }
